1b01bb79670ddd4854ad5e4f5fe6897304bf3ce0
Steveh77 <stederosa76@gmail.com>
2022-06-20 16:15:07 +0200

    added script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // chiedi all'utente il suo nome
 // poi chiedi il suo cognome
-// poi chiedi il suo cognome preferito
+// poi chiedi il suo colore preferito
 // infine scrivi nella pagina nomecognomecolorepreferito21
+
+
+const password = document.getElementById('result')
+
+const Name = prompt("come ti chiami");
+console.log(Name);
+
+const lastName = prompt("qual è il tuo cognome?")
+console.log(lastName);
+
+const favouriteColor = prompt("qual è il tuo colore preferito?")
+console.log(favouriteColor)
+
+password.innerText = Name + lastName + favouriteColor + "21"
 
